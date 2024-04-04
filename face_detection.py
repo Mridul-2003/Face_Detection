468a158cf9a49238c3164fb03969d3eb450e98f6
Mridul-2003 <110475111+Mridul-2003@users.noreply.github.com>
2024-04-04 16:19:47 +0530

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -1,25 +1,43 @@
 import cv2
+
+# Load the cascade for face detection.
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
+# Specify the source of the video.
+source = "/Users/useradmin/Desktop/opencv/Goku doing job of security guard during the party.mp4"
+cap = cv2.VideoCapture(source)
 
-cap = cv2.VideoCapture(0)
+# Check if the video source has been opened successfully.
+if not cap.isOpened():
+    print("Error opening video source")
+    exit()
+
+win_name = 'Camera Preview'
+cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)  # Use WINDOW_NORMAL to allow resizing the window.
 
 while True:
-    ret,frame = cap.read()
+    ret, frame = cap.read()
     if not ret:
+        print("Failed to grab frame")
         break
 
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    # Convert the frame to grayscale for face detection.
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30,30))
+    # Detect faces in the image.
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=11, minSize=(20, 20))
 
-    for(x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+    # Draw rectangles around the faces.
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-    cv2.imshow('Face Detection',frame)
+    # Display the frame with detected faces.
+    cv2.imshow(win_name, frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # Break the loop when 'ESC' is pressed.
+    if cv2.waitKey(1) & 0xFF == 27:  # 27 is the ASCII code for 'ESC'
         break
 
+# Release the video capture object and close all OpenCV windows.
 cap.release()
 cv2.destroyAllWindows()
